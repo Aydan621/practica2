d81ecad0066ef6ec7f8f473011f8d615c6f003c6
Aydan Charynchyk <caryncykajdan@gmail.com>
2024-04-18 16:48:59 +0700

    first

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int choice;
+        do
+        {
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Введите первое число:");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите второе число:");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Сумма: {num1 + num2}");
+                    break;
+                case 2:
+                    Console.WriteLine("Введите уменьшаемое число:");
+                    int minuend = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите вычитаемое число:");
+                    int subtrahend = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Разность: {minuend - subtrahend}");
+                    break;
+                case 3:
+                    Console.WriteLine("Введите первый множитель:");
+                    int factor1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите второй множитель:");
+                    int factor2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Произведение: {factor1 * factor2}");
+                    break;
+                case 4:
+                    Console.WriteLine("Введите делимое число:");
+                    int dividend = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите делитель:");
+                    int divisor = Convert.ToInt32(Console.ReadLine());
+                    if (divisor != 0)
+                    {
+                        Console.WriteLine($"Частное: {dividend / divisor}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Деление на ноль невозможно.");
+                    }
+                    break;
+            }
+        }
+        while (choice != 4);
+    }
+}
